ac4763354497240750d3d5c349e775179080258c
Al Elliott <adrelliott@gmail.com>
2022-01-18 17:02:07 +0100

    Tweaks to nav

--- resources/views/partials/common/nav/link.blade.php
+++ resources/views/partials/common/nav/link.blade.php
@@ -3,7 +3,7 @@
         $selected = "border-oblong-blue-dark";
 @endphp 
 
-<a class="xw-1/3 mx-4 py-2 md:pt-2 mt-2 text-gray-500 md:mt-0 uppercase font-extralight font-sans border-b-2 border-white hover:border-oblong-blue-dark focus:outline-none focus:shadow-outline {{  $selected ?? '' }}"
+<a class="xw-1/3 mx-4 py-2 md:pt-2 mt-2 text-oblong-blue-dark md:mt-0 uppercase font-extralight font-sans border-b-2 border-white hover:border-oblong-blue-dark focus:outline-none focus:shadow-outline {{  $selected ?? '' }}"
     href="{{ $link ?? 'Define a link' }}"
     >
     {{ $label ?? 'Define a label' }}
